d7bd9fd0fe4c08cfa013f55410313a29e34f5646
agung <agung@digitalsekuriti.id>
2025-02-19 13:21:46 +0700

    feat: add onchange search input & edit naming apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,65 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:async';  // Import dart:async untuk Timer
+import 'dart:convert';  // Import dart:convert untuk json.decode
+import 'package:flutter/material.dart';   // Import material.dart untuk MaterialApp dan Widget
+import 'package:http/http.dart' as http;  // Import http.dart untuk http request
+import 'package:cached_network_image/cached_network_image.dart';  // Import cached_network_image.dart untuk CachedNetworkImage
+import 'package:flutter_dotenv/flutter_dotenv.dart';  // Import flutter_dotenv.dart untuk dotenv
 
+// Fungsi main async untuk menjalankan aplikasi
 void main() async {
+  // Load file .env
   await dotenv.load(fileName: ".env");
+  // Run aplikasi
   runApp(MyApp());
 }
 
+// MyApp untuk menampilkan aplikasi NOFI: Movie App
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // MaterialApp dengan tema dan home page
     return MaterialApp(
+      // Judul aplikasi
       title: 'NOFI: Movie App',
+      // Tema aplikasi
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      // HomePage sebagai home page
       home: HomePage(),
+      // Routes untuk navigasi ke MovieDetailPage
       routes: {MovieDetailPage.routeName: (context) => MovieDetailPage()},
     );
   }
 }
 
+// HomePage untuk menampilkan list movie
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  // Variabel state
   List movies = [];
   int currentPage = 1;
   String currentQuery = '';
   int? selectedGenre;
   bool hasMore = true;
   bool isLoading = false;
+
+  // Deklarasikan Timer untuk debounce search
+  Timer? _debounce;
+
+  // Controller untuk search
   TextEditingController searchController = TextEditingController();
+  // Get API Key dari file .env
   final String apiKey = dotenv.env['TMDB_API_KEY'] ?? '';
+  // ScrollController untuk load more
   final ScrollController _scrollController = ScrollController();
 
-  // Data genre lokal
+  // Deklarasikan genres sebagai List<Map<String, dynamic>>
   final List<Map<String, dynamic>> genres = [
     {"id": 28, "name": "Action"},
     {"id": 12, "name": "Adventure"},
@@ -63,20 +82,26 @@ class _HomePageState extends State<HomePage> {
     {"id": 37, "name": "Western"},
   ];
 
+  // Fungsi initState untuk memanggil fetchMovies
   @override
   void initState() {
+    // Panggil initState dari parent
     super.initState();
+    // Fetch data movie
     fetchMovies();
   }
 
   /// Fungsi untuk mengambil data movie.
   /// Parameter [isLoadMore] digunakan untuk menambahkan data pada list.
   Future<void> fetchMovies({bool isLoadMore = false}) async {
+    // Jika isLoading maka return
     if (isLoading) return;
+    // Set isLoading=true
     setState(() {
       isLoading = true;
     });
 
+    // Deklarasikan URL kosong
     String url = '';
 
     // URL berdasarkan: search, filter genre, atau default discover
@@ -91,18 +116,24 @@ class _HomePageState extends State<HomePage> {
           'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=$apiKey&page=$currentPage';
     }
 
+    // Fetch data dari API
     try {
       final response = await http.get(Uri.parse(url));
+      // Jika response OK (status code 200)
       if (response.statusCode == 200) {
+        // Decode response body
         final data = json.decode(response.body);
+        // Set newMovies sebagai data['results']
         List newMovies = data['results'];
 
+        // Jika newMovies kurang dari 20 maka hasMore=false
         if (newMovies.length < 20) {
           hasMore = false;
         } else {
           hasMore = true;
         }
 
+        // Set state movies dengan newMovies
         setState(() {
           if (isLoadMore) {
             movies.addAll(newMovies);
@@ -111,22 +142,60 @@ class _HomePageState extends State<HomePage> {
           }
         });
       } else {
+        // Jika response error maka tampilkan error message
         print('Error: ${response.statusCode}');
       }
     } catch (e) {
+      // Jika terjadi error maka tampilkan error message
       print('Error fetching movies: $e');
+
+    // Finally for set isLoading=false
     } finally {
+      // Set isLoading=false
       setState(() {
         isLoading = false;
       });
     }
   }
 
+  // Fungsi untuk load more data
   void loadMore() {
+    // currentPage++ is current page + 1
     currentPage++;
+    // Fetch data and set isLoadMore=true
     fetchMovies(isLoadMore: true);
   }
 
+  // Fungsi untuk debounce search
+  void onSearchChanged(String query) {
+    // Jika debounce active maka cancel
+    // Lalu set debounce dengan Timer 500ms
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(Duration(milliseconds: 500), () {
+      // Jika query tidak kosong maka panggil onSearch
+      if (query.isNotEmpty) {
+        onSearch();
+      } else {
+        // Jika query kosong, kembalikan ke keadaan default
+        // Reset state
+        currentPage = 1;
+        currentQuery = '';
+        setState(() {
+          selectedGenre = null;
+        });
+
+        // Fetch data
+        fetchMovies();
+        _scrollController.animateTo(
+          0,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
+
+  // Fungsi untuk search
   void onSearch() {
     // Reset state
     currentPage = 1;
@@ -134,6 +203,8 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       selectedGenre = null;
     });
+
+    // Fetch data
     fetchMovies();
 
     // Kembali ke posisi atas
@@ -144,13 +215,17 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // Fungsi untuk memilih genre
   void onSelectGenre(int? genreId) {
+    // Reset state
     setState(() {
       selectedGenre = genreId;
       currentQuery = '';
       searchController.clear();
       currentPage = 1;
     });
+
+    // Fetch data
     fetchMovies();
 
     // Kembali ke posisi atas
@@ -161,8 +236,10 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // Build UI
   @override
   Widget build(BuildContext context) {
+    // Scaffold dengan AppBar dan body
     return Scaffold(
       appBar: AppBar(title: Text('Movies')),
       body: Column(
@@ -178,8 +255,10 @@ class _HomePageState extends State<HomePage> {
                     hint: Text('Select Genre'),
                     value: selectedGenre,
                     isExpanded: true,
+                    // DropdownMenuItem untuk setiap genre
                     items: [
-                      DropdownMenuItem<int?>(child: Text('All'), value: null),
+                      // DropdownMenuItem untuk "All Genre"
+                      DropdownMenuItem<int?>(child: Text('All Genre'), value: null),
                       ...genres.map((genre) {
                         return DropdownMenuItem<int?>(
                           child: Text(genre['name']),
@@ -187,6 +266,7 @@ class _HomePageState extends State<HomePage> {
                         );
                       }).toList(),
                     ],
+                    // Fungsi onChanged untuk onSelectGenre
                     onChanged: (value) {
                       onSelectGenre(value);
                     },
@@ -196,7 +276,13 @@ class _HomePageState extends State<HomePage> {
                 // Field search
                 Expanded(
                   child: TextField(
+                    // Search Controller
                     controller: searchController,
+                    // Fungsi onSearchChanged untuk debounce search
+                    onChanged: onSearchChanged,
+                    // Fungsi onSearch untuk search
+                    onSubmitted: (value) => onSearch(),
+                    // Dekorasi input field
                     decoration: InputDecoration(
                       hintText: 'Search Movies',
                       suffixIcon: IconButton(
@@ -215,15 +301,20 @@ class _HomePageState extends State<HomePage> {
             child: GridView.builder(
               controller: _scrollController,
               padding: const EdgeInsets.all(8.0),
+              // SliverGridDelegateWithFixedCrossAxisCount untuk 2 kolom
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 childAspectRatio: 0.65,
                 crossAxisSpacing: 8,
                 mainAxisSpacing: 8,
               ),
+              // Jumlah item sesuai jumlah movie
               itemCount: movies.length,
+              // ItemBuilder untuk menampilkan item movie
               itemBuilder: (context, index) {
+                // Movie pada index tertentu
                 final movie = movies[index];
+                // URL poster image
                 final posterPath = movie['poster_path'];
                 final imageUrl =
                     posterPath != null
@@ -238,7 +329,10 @@ class _HomePageState extends State<HomePage> {
                     4,
                   );
                 }
+                // Rating movie
                 final voteAverage = (movie['vote_average'] ?? 0.0) as num;
+                
+                // InkWell untuk menavigasi ke MovieDetailPage
                 return InkWell(
                   onTap: () {
                     Navigator.pushNamed(
@@ -249,7 +343,7 @@ class _HomePageState extends State<HomePage> {
                   },
                   child: Stack(
                     children: [
-                      // Poster Image (Cached)
+                      // Poster Image menggunakan Cached
                       CachedNetworkImage(
                         imageUrl: imageUrl,
                         width: double.infinity,
@@ -321,8 +415,10 @@ class _HomePageState extends State<HomePage> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0),
             child:
+            // Jika isLoading maka tampilkan CircularProgressIndicator
                 (isLoading)
                     ? CircularProgressIndicator()
+                    // Jika hasMore maka tampilkan tombol "Show More"
                     : (hasMore
                         ? ElevatedButton(
                           onPressed: loadMore,
@@ -336,6 +432,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+// MovieDetailPage untuk menampilkan detail movie
 class MovieDetailPage extends StatefulWidget {
   static const routeName = '/movieDetail';
 
@@ -344,10 +441,12 @@ class MovieDetailPage extends StatefulWidget {
 }
 
 class _MovieDetailPageState extends State<MovieDetailPage> {
+  // Variabel state
   Map? movieDetail;
   bool isLoading = true;
   final String apiKey = dotenv.env['TMDB_API_KEY'] ?? '';
 
+  // Fungsi didChangeDependencies untuk memanggil fetchMovieDetail
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -356,50 +455,62 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
     fetchMovieDetail(movieId);
   }
 
-  /// Mengambil detail movie berdasarkan movieId
+  // Mengambil detail movie berdasarkan movieId
   Future<void> fetchMovieDetail(int movieId) async {
     final url = 'https://api.themoviedb.org/3/movie/$movieId?api_key=$apiKey';
     try {
       final response = await http.get(Uri.parse(url));
+      // Jika response OK (status code 200)
       if (response.statusCode == 200) {
+        // Decode response body
         final data = json.decode(response.body);
+        // Set state movieDetail dan isLoading=false
         setState(() {
           movieDetail = data;
           isLoading = false;
         });
       } else {
+        // Jika response error maka tampilkan error message
         print('Error fetching detail: ${response.statusCode}');
       }
     } catch (e) {
+      // Jika terjadi error maka tampilkan error message
       print('Error: $e');
     }
   }
 
+  // Build UI
   @override
   Widget build(BuildContext context) {
+    // Scaffold dengan AppBar dan body
     return Scaffold(
       appBar: AppBar(title: Text('Movie Detail')),
       body:
+      // Jika isLoading maka tampilkan CircularProgressIndicator
           isLoading
               ? Center(child: CircularProgressIndicator())
+              // Jika movieDetail null maka tampilkan pesan "No data available"
               : movieDetail == null
               ? Center(child: Text('No data available'))
+              // Jika movieDetail ada maka tampilkan detail movie
               : SingleChildScrollView(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      // Poster
+                      // Poster Image menggunakan Cached
                       Center(
                         child: CachedNetworkImage(
                           imageUrl:
+                              // Check jika poster_path null maka tampilkan No Image 
                               movieDetail!['poster_path'] != null
                                   ? 'https://image.tmdb.org/t/p/w300${movieDetail!['poster_path']}'
                                   : 'https://dummyimage.com/300x450/000/fff&text=No+Image',
                           height: 450,
                           fit: BoxFit.cover,
                           placeholder:
+                              // Placeholder loading
                               (context, url) => Container(
                                 height: 450,
                                 child: Center(
@@ -407,6 +518,7 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                                 ),
                               ),
                           errorWidget:
+                              // Replacement icon jika error
                               (context, url, error) => Container(
                                 height: 450,
                                 color: Colors.grey[300],
@@ -434,7 +546,7 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                                 .toStringAsFixed(1),
                             style: TextStyle(fontSize: 16),
                           ),
-                          // Info lainnya seperti release date dan runtime
+                          // Info lain seperti release date dan runtime
                           SizedBox(width: 16),
                           Text(
                             'Release: ${movieDetail!['release_date'] ?? 'N/A'}',
